8e5793db2552358612adb454059ea453ea76f0c4
Manuel Jesús Aguiar Gámez <manuelaguiargamez@gmail.com>
2024-03-06 16:06:18 -0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Tuxretas | Administrador de ligas de futbol</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Administrador de ligas de futbol" />
+        <meta content="Themesbrand" name="author" />
+
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link href="assets/libs/jsvectormap/css/jsvectormap.css" rel="stylesheet" type="text/css" />
+        <link href="assets/libs/swiper/swiper-bundle.css" rel="stylesheet" type="text/css" />
+
+        <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css"/>
+        <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css"/>
+        <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css"/>
+        <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet" type="text/css"/>
+        @livewireStyles
+		@stack('custom_styles')
+    </head>
+    <body>
+
+        <div id="layout-wrapper">
+            @include('layouts.header')
+            @include('layouts.menu')
+            <div class="vertical-overlay"></div>
+            <div class="main-content">
+                <div class="page-content">
+                    {{ $slot }}	
+                </div>
+            </div>
+        </div>
+
+        @livewireScripts
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+        <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+        <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins.js') }}"></script>
+        <script src="{{ asset('assets/js/app.js') }}"></script>
+        @stack('js')
+
+		<script>
+			@stack('scripts')			
+		</script>
+    </body>
+</html>
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Tuxretas</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Administrador de Ligas de Fútbol" name="description" />
+    <meta content="Themesbrand" name="author" />
+
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico"') }}">
+
+    <link href="{{ asset('assets/js/layout.js') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+    
+    <div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+        <div class="bg-overlay"></div>
+        <div class="auth-page-content overflow-hidden pt-lg-5">
+            <div class="container">
+                @yield('content')
+
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0">&copy;
+                                <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+
+    <script src="{{ asset('assets/js/pages/password-addon.init.js') }}"></script>
+</body>
+
+</html>
